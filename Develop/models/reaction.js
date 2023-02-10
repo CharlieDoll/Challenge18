4a29494e318a10b304cfd334adbf91d72abfb357
CharlieDoll <113851328+CharlieDoll@users.noreply.github.com>
2023-02-10 13:58:50 +0000

    reaction js

---
+++ Develop/models/reaction.js
@@ -0,0 +1,28 @@
+const { Schema, Types, model } = require("mongoose");
+
+const reactionSchema = new Schema({
+  reactionID: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  toJSON: {
+    getters: true,
+  },
+  id: false,
+});
+
+const Reaction = model("Reaction", reactionSchema);
+module.exports = Reaction;
